02da0a01c38f46f03db6bde9c65018b6dc93c468
arthurassists <apetbrz@gmail.com>
2024-08-01 23:04:54 -0400

    init commit. functional-ish

---
+++ src/budget.rs
@@ -0,0 +1,214 @@
+use std::collections::HashMap;
+
+const AUTOMATIC_PAYMENT_PREFIX: char = '*';
+
+pub struct Budget{
+    username: String,
+    expected_income: i32,
+    pub current_balance: i32,
+    expected_expenses: HashMap<String, i32>,
+    current_expenses: HashMap<String, i32>,
+    savings: i32
+}
+impl Budget{
+    //new(): factory method, returning a new Budget
+    //TODO: SAVING/LOADING, USERNAMES
+    pub fn new() -> Budget{
+        Budget{
+            username: String::from("noname"),
+            expected_income: 0,
+            current_balance: 0,
+            expected_expenses: HashMap::new(),
+            current_expenses: HashMap::new(),
+            savings: 0
+        }
+    }
+    
+    //set_income(): sets expected_income to the new value
+    pub fn set_income(&mut self, cents: i32){
+        self.expected_income = cents;
+    }
+    
+    //get_paid(): adds expected_income to current_balance
+    pub fn get_paid(&mut self) -> Result<String, String> {
+        self.refresh();
+        self.current_balance += self.expected_income;
+        match self.make_automatic_payments(self.current_balance){
+            Ok(n) => {
+                if n == -1 { Ok(String::new()) }
+                else{ Ok(String::from("Payments made!")) }
+            },
+            Err(n) => Err(String::from("You couldn't afford your automatic payments!"))
+        }
+    }
+    
+    //get_paid_value(): adds given value to current_balance
+    pub fn get_paid_value(&mut self, cents: i32){
+
+        self.current_balance += cents;
+    }
+    
+    //refresh(): resets current_expenses
+    pub fn refresh(&mut self){
+        for key in self.current_expenses.iter_mut(){
+            *key.1 = 0;
+        }
+    }
+
+    //make_automatic_payments(): adds up total of automatic payments, returns money left over (if positive -> Ok, if negative -> Err)
+    pub fn make_automatic_payments(&mut self, cents: i32) -> Result<i32, i32>{
+        
+        let mut autos: Vec<String> = Vec::new();
+        let mut payment = 0;
+        for key in self.expected_expenses.iter(){
+            if key.0.chars().next().unwrap() == AUTOMATIC_PAYMENT_PREFIX{
+                autos.push(key.0.clone());
+                payment += key.1;
+            }
+        }
+
+        if payment == 0 {
+            return Ok(-1)
+        }
+
+        payment = cents - payment;
+
+        if payment < 0 {
+            Err(payment)
+        }else{
+            for name in autos{
+                let _ = self.make_static_payment(&name);
+            }
+            Ok(payment)
+        }
+    }
+
+    //add_expense(): creates a new expense in both HashMaps, with the new value as the expected value in expected_expenses
+    pub fn add_expense(&mut self, name: &str, cents: i32){
+        self.expected_expenses.insert(name.to_string().to_ascii_lowercase(), cents);
+        self.current_expenses.insert(name.to_string().to_ascii_lowercase(), 0);
+    }
+    
+    //make_static_payment(): makes a payment into current_expenses, with the value from expected_expenses
+    pub fn make_static_payment(&mut self, name: &str) -> Result<String, String>{
+        let amount = if let Some(n) = self.expected_expenses.get(name){
+            n.clone()
+        }else{
+            return Err(String::from("expense-not-found"))
+        };
+
+        self.make_dynamic_payment(name, amount)
+    }
+    
+    //make_dynamic_payment(): makes a payment into current_expenses, with the given value
+    pub fn make_dynamic_payment(&mut self, name: &str, cents: i32) -> Result<String, String>{
+        let name = name.to_ascii_lowercase();
+        if let Some(n) = self.current_expenses.get_mut(&name){
+            self.current_balance -= cents;
+            *n += cents;
+        }else{
+            return Err(String::from("expense-not-found"))
+        };
+
+        Ok(format!("Payment made: {} to {}", format_dollars(&cents), to_title_case(name)))
+    }
+
+    //save(): adds the given amount into savings
+    pub fn save(&mut self, cents: i32) -> Result<String, String>{
+        if self.current_balance < cents {
+            Err(String::from("Not enough in balance to save that much!"))
+        }else{
+            self.current_balance -= cents;
+            self.savings += cents;
+            Ok(format!("{} saved!", format_dollars(&cents)))
+        }
+    }
+
+    //save_all(): moves current_balance to savings
+    pub fn save_all(&mut self) -> Result<String, String>{
+        self.save(self.current_balance)
+    }
+}
+
+impl std::fmt::Display for Budget{
+    //fmt(): Display String has a header, with username, followed by balance and expected pay, and then all expenses
+    //TODO: better?? lol
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "==={{ Welcome, {}! }}===\n", self.username)?;
+        write!(f, "Balance: {}\n", format_dollars(&self.current_balance))?;
+        write!(f, "Income: {}\n", format_dollars(&self.expected_income))?;
+        write!(f, "Savings: {}\n", format_dollars(&self.savings))?;
+        write!(f, "\nExpenses:\n")?;
+
+        let mut loop_output: std::fmt::Result = Ok(());
+
+        for key in self.expected_expenses.iter(){
+            let current_amount = self.current_expenses.get(key.0).expect(format!("{}-missing-from-current-expenses",key.0).as_str());
+            let category_name = to_title_case(key.0.clone());
+
+            loop_output = write!(f,"{}: {}/{}\n", category_name, format_dollars(current_amount), format_dollars(key.1));
+            if loop_output.is_err() {break;}
+        }
+
+        loop_output
+    }
+}
+
+//format_dollars(): takes an amount of cents and formats it to ${X}+.XX
+pub fn format_dollars(cents: &i32) -> String {
+    let cents = {
+        cents.to_string()
+    };
+    let dollars = match cents.len(){
+        3.. => cents.split_at(cents.len()-2),
+        2 => ("0", cents.as_str()),
+        1 => ("0", &format!("0{}",cents)[..]),
+        _ => ("0", "00")
+    };
+    let mut output = String::from("$");
+    output.push_str(dollars.0);
+    output.push('.');
+    output.push_str(dollars.1);
+    output.to_string()
+}
+
+//dollars_to_cents(): takes a decimal amount of dollars and returns it in integer cents
+pub fn dollars_to_cents(dollars: f32) -> i32 {
+    (dollars * 100.0) as i32
+}
+
+//parse_dollar_string(): takes a string literal and returns an integer cent amount if valid, or error message if not
+pub fn parse_dollar_string(s: &str) -> Result<i32, String>{
+    if s.is_empty(){
+        return Err(String::from("empty-dollar-string"));
+    }
+    let mut s = s;
+    if s.chars().into_iter().next().unwrap() == '$' {
+        s = &s[1..];
+    }
+    match s.parse::<i32>(){
+        Ok(n) => Ok(n*100),
+        Err(er) => {
+            match s.parse::<f32>(){
+                Ok(m) => Ok(dollars_to_cents(m)),
+                Err(err) => Err(String::from("not-a-number"))
+            }
+        }
+    }
+}
+
+//to_title_case(): takes a String and returns a new String with the first letter uppercase, and the rest lowercase
+pub fn to_title_case(s: String) -> String {
+    let mut out = s.clone();
+    if let Some(r) = out.get_mut(0..1) {
+        if r == "*" {
+            if let Some(s) = out.get_mut(1..2){
+                s.make_ascii_uppercase();
+            }
+        }
+        else{
+            r.make_ascii_uppercase();
+        }
+    }
+    out.clone()
+}
---
+++ src/main.rs
@@ -0,0 +1,146 @@
+mod budget;
+
+use budget::Budget;
+use std::io;
+use console::Term;
+use dialoguer::Input;
+
+const APP_TITLE: &str = "nos-clbt";
+const COMMAND_ARGS_LIMIT: usize = 10;
+const COMMAND_PROMPT: &str = ">>";
+const COMMANDS_LIST: &str ="========================={ nos-clbt }=========================\n\
+                            ======{ everything in [square brackets] is an argument }======\n\
+                            \thelp: shows this menu, lol!\n\
+                            \tset income [amount]: sets your expected income\n\
+                            \tpaid: receive your income\n\
+                            \tpaid [amount]: receive some amount\n\
+                            \tnew [name] [amount]: create a new expenditure\n\
+                            \t\t(overrides existing copies)\n\
+                            \t\t(prefix with \"*\" to make it automatic)\n\
+                            \tpay [name]: pay a static expenditure\n\
+                            \tpay [name] [amount]: pay some amount to an expenditure\n\
+                            \tsave [amount]: add an amount into savings\n\
+                            \tsave all: add the remaining balance into savings\n\
+                            \tclear: clear the terminal\n\
+                            \texit: close the app\n\
+                            ==============================================================\n\
+                            \tMANY FEATURES ARE NOT IMPLEMENTED!!!!\n\
+                            \tINCLUDING SAVING/LOADING!!!!!!!!!!!!!\n\
+                            ==============================================================\n";
+
+fn main() -> Result<(), io::Error> {
+    let term = Term::stdout();
+    term.set_title(APP_TITLE);
+    term.clear_screen()?;
+    fn output(t: &Term, s: &str){
+        t.write_line(s).expect("console-should-write");
+    }
+
+    //output(&term, "initializing...");
+
+    let mut bud = Budget::new();
+
+    output(&term, &bud.to_string());
+    output(&term, ">>  tip: enter 'help' to get started!\n");
+
+    let mut user_input: String;
+    loop{
+        user_input = Input::<String>::new()
+            .with_prompt(COMMAND_PROMPT)
+            .interact_text()
+            .unwrap();
+
+        if user_input.trim() == "exit" { break; }
+
+        
+        let result = parse_command(&term, &user_input, &mut bud);
+        
+        term.clear_screen()?;
+
+        output(&term, &bud.to_string());
+
+        match result{
+            Ok(s) => output(&term, &s),
+            Err(s) => {
+                output(&term, "error!");
+                output(&term, &s);
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn parse_command(term: &Term, input: &str, bud: &mut Budget) -> Result<String, String>{
+    let mut command = input.split_ascii_whitespace();
+    let command: [&str; COMMAND_ARGS_LIMIT] = [(); COMMAND_ARGS_LIMIT].map(|_| command.next().unwrap_or(""));
+
+    let out: Result<String, String> = {
+        match command[0]{
+            "help" => {
+                Ok(COMMANDS_LIST.to_string())
+            },
+            "set" => {
+                match command[1]{
+                    "income" => {
+                        let amount = budget::parse_dollar_string(command[2])?;
+                        bud.set_income(amount);
+                        Ok("Input set!".to_string())
+                    },
+                    _ => {
+                        Err(String::from("invalid-command"))
+                    }
+                }
+            },
+            "paid" => {
+                match command[1]{
+                    "" => {
+                        let output = bud.get_paid()?;
+                        Ok(format!("You got paid!\n{}",output))
+                    },
+                    _ => {
+                        let amount = budget::parse_dollar_string(command[2])?;
+                        bud.get_paid_value(amount);
+                        Ok(format!("You got paid {}!", amount))
+                    }
+                }
+            },
+            "new" => {
+                let name = command[1];
+                let amount = budget::parse_dollar_string(command[2])?;
+                bud.add_expense(name, amount);
+                Ok(String::from("Expense added!"))
+            },
+            "pay" => {
+                let name = command[1];
+                match command[2]{
+                    "" => bud.make_static_payment(name),
+                    _ =>{
+                        let amount = budget::parse_dollar_string(command[2])?;
+                        bud.make_dynamic_payment(name, amount)
+                    }
+                }
+            },
+            "save" => {
+                match command[1]{
+                    "" => Err(String::from("invalid-command")),
+                    "all" => bud.save_all(),
+                    _ => {
+                        let amount = budget::parse_dollar_string(command[2])?;
+                        bud.save(amount)
+                    }
+                }
+            },
+            "clear" => {
+                match term.clear_screen(){
+                    Ok(()) => Ok(String::new()),
+                    Err(e) => Err(e.to_string())
+                }
+            },
+            _ => return Err(String::from("invalid-command"))
+        }
+    };
+
+    out
+}
+
