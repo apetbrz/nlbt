7a880b55dd453a29aeb3ca4ed06be52db662a957
Arthur Petroff Desktop <apetbrz@gmail.com>
2025-07-22 16:04:02 -0400

    began work on file system

--- src/fileio.rs
+++ src/fileio.rs
@@ -1,16 +1,30 @@
-use crate::budget;
-use std::fs::File;
+use std::fs::{self, File};
+use std::io;
+use std::path;
 
-fn get_account_file(account: Option<&String>) -> Option<File> {
-    None
-}
-fn make_account_file(account: &String) -> File {
-    todo!()
-}
+//OS-dependent save location
+const SAVE_DIR: &str = {
+    #[cfg(target_os = "windows")]
+    "%APPDATA%/.nclbt";
+    #[cfg(target_os = "linux")]
+    "$HOME/.local/share/nclbt"
+};
 
-pub fn load_bud(account: Option<&String>) -> budget::Budget{
-    match get_account_file(account.clone()) {
-        Some(file) => todo!(),
-        None => budget::Budget::new(account)
+//only returns a file if it exists, user must intentionally opt-in to making a new account
+fn access_account_file(account: Option<&str>) -> Result<File, io::Error> {
+    let account = account.unwrap_or("default");
+    File::open(format!("{SAVE_DIR}{account}.bdgt"))
+}
+fn make_default_account_file() -> Result<File, io::Error> {
+    make_account_file("default")
+}
+fn make_account_file(account: &str) -> Result<File, io::Error> {
+    if fs::read_dir(SAVE_DIR).is_err() {
+        fs::create_dir_all(
+            path::absolute(SAVE_DIR)
+                .unwrap_or_else(|_| panic!("SAVE_DIR constant invalid! {SAVE_DIR}")),
+        )
+        .unwrap();
     }
+    File::create_new(format!("{SAVE_DIR}{account}.gv"))
 }
